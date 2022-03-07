259b5345e19bcdca2d27400aef5a68dc01951bc1
jyb1798 <bestdung8509@gmail.com>
2022-03-08 00:58:49 +0900

    Add : 초기세팅

---
+++ components/comment/script.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Utterances = () => {
+  return (
+    <section
+      ref={(elem) => {
+        if (!elem) {
+          return
+        }
+        const scriptElem = document.createElement('script')
+        scriptElem.src = 'https://utteranc.es/client.js'
+        scriptElem.async = true
+        scriptElem.setAttribute('repo', 'Fetudy/Fetudys_Blog')
+        scriptElem.setAttribute('issue-term', 'title')
+        scriptElem.setAttribute('theme', 'github-light')
+        scriptElem.crossOrigin = 'anonymous'
+        elem.appendChild(scriptElem)
+      }}
+    />
+  )
+}
+
+export default Utterances
