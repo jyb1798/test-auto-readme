da45beea96b4db54ed243d1f4058fa138f1437fb
jyb1798 <bestdung8509@gmail.com>
2022-03-08 01:02:14 +0900

    Modify: Utterances 수정

--- components/comment/script.js
+++ components/comment/script.js
@@ -10,7 +10,7 @@ const Utterances = () => {
         const scriptElem = document.createElement('script')
         scriptElem.src = 'https://utteranc.es/client.js'
         scriptElem.async = true
-        scriptElem.setAttribute('repo', 'Fetudy/Fetudys_Blog')
+        scriptElem.setAttribute('repo', 'Fetudy/Fetudy_Blog')
         scriptElem.setAttribute('issue-term', 'title')
         scriptElem.setAttribute('theme', 'github-light')
         scriptElem.crossOrigin = 'anonymous'
