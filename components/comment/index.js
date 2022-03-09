ff89c8a88e5950f8a9a9a124e5a9c13eb0a4f2cc
zerochae <zerochae@kakao.com>
2022-03-09 17:50:19 +0900

    Modify: comment css

--- components/comment/index.js
+++ components/comment/index.js
@@ -2,7 +2,18 @@ import Utterances from './script'
 
 function Comment() {
   return (
-    <div>
+    <div style={{ position: 'relative', left: '-30px' }}>
+      <br />
+      <hr
+        style={{
+          margin: '40px 0',
+          height: '3px',
+          border: 'none',
+          backgroundColor: '#ddd',
+          backgroundImage:
+            'repeating-linear-gradient(-45deg, #fff, #fff 4px, transparent 4px, transparent 8px)',
+        }}
+      />
       <Utterances />
     </div>
   )
